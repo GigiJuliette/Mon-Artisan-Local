67649308a888b33b0b1b528d05c67de2606b7c85
Juliette De Meo <gigialiasjuliette@gmail.com>
2026-03-08 16:48:25 +0100

    Shadcn config

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,6 @@
-import { Button } from "../components/Button/Button";
-
 export const Home = () => {
   return (
     <>
-      <Button type="button" variant="primary">
-        Yesyes
-      </Button>
       <p>eibkfjnefoeknf hellooooo world</p>
     </>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { useLoginFetch } from "../hooks/useLoginFetch";
 import { authService } from "../services/authService";
 import type { User } from "../types/User";
+import { Input } from "@/components/ui/input";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
 
 export const Login = () => {
   const [formData, setFormData] = useState<User>({ email: "", password: "" });
@@ -17,28 +20,36 @@ export const Login = () => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-          disabled={loading}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Password"
-          value={formData.password}
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
-          disabled={loading}
-          required
-        />
-        {error && <div>{error}</div>}
-        <button type="submit" disabled={loading}>
+        <Field>
+          <FieldLabel>Email</FieldLabel>
+          <Input
+            type="email"
+            placeholder="exemple@gi-gi.dev"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+            disabled={loading}
+            required
+          />
+        </Field>
+        <Field>
+          <FieldLabel>Password</FieldLabel>
+          <Input
+            type="text"
+            placeholder="Password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            disabled={loading}
+            required
+          />
+        </Field>
+        {error && <span className="sec-clr">{error}</span>}
+        <Button type="submit" variant="outline" disabled={loading}>
           {loading ? "Connexion en cours..." : "Se connecter"}
-        </button>
+        </Button>
       </form>
     </>
   );
